600837b011e4ee08f56f952e3825b37c3c5999e5
Dipesh B C <bcdipeshwork@gmail.com>
2024-01-30 11:34:38 -0500

    Initial setup

---
+++ backend/.flaskenv
@@ -0,0 +1,3 @@
+FLAKS_APP="memorable-messages.py"
+SECRET_KEY="you don't know it"
+DATABASE_URI="postgresql://postgres:pgAdmin@localhost/memorable_messages"
---
+++ backend/app/models.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timezone
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), unique=True)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    created_at: so.Mapped[datetime] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc)
+    )
+
+    occasions: so.WriteOnlyMapped["Occasion"] = so.relationship(back_populates="user")
+
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+
+class Occasion(db.Model):
+    __tablename__ = "occasions"
+
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id))
+    delivery_method: so.Mapped[str] = so.mapped_column(sa.String(64))
+    occasion_type: so.Mapped[str] = so.mapped_column(sa.String(256))
+    message_content: so.Mapped[str] = so.mapped_column(sa.Text())
+    date_time: so.Mapped[datetime] = so.mapped_column(sa.DateTime())
+    created_at: so.Mapped[datetime] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc)
+    )
+
+    user: so.Mapped[User] = so.relationship(back_populates="occasions")
+    delivery_histories: so.WriteOnlyMapped["DeliveryHistory"] = so.relationship(
+        back_populates="occasion"
+    )
+
+    def __repr__(self):
+        return f"<Occasion {self.occasion_type}>"
+
+
+class DeliveryHistory(db.Model):
+    __tablename__ = "delivery_histories"
+
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    occasion_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(Occasion.id))
+    status: so.Mapped[str] = so.mapped_column(sa.String(64))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc)
+    )
+
+    occasion: so.Mapped[Occasion] = so.relationship(back_populates="delivery_histories")
+
+    def __repr__(self):
+        return f"<DeliveryHistory {self.status}>"
