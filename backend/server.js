8fcc1ed152425793e579b0051c66d77c796dd702
Dipesh B C <bcdipeshwork@gmail.com>
2024-06-03 22:20:22 -0400

    Use environment variables for db config

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,15 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Configure database connection
-const sequelize = new Sequelize("dipeshbc", "dipeshbc", "postgres", {
-  host: "localhost",
-  dialect: "postgres",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  },
+);
 
 // Test the connection
 sequelize
