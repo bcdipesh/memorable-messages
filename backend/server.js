639606a7e3dc2e2764100df756d4cdf4a82a89c9
Dipesh B C <bcdipeshwork@gmail.com>
2024-06-04 18:58:48 -0400

    Use db connection string

--- backend/server.js
+++ backend/server.js
@@ -13,15 +13,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Configure database connection
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-  },
-);
+const sequelize = new Sequelize(`${process.env.DB_CONNECTION_STRING}`);
 
 // Test the connection
 sequelize
